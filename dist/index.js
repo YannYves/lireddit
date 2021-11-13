4d9db42eceb76959a4cd051bf599354bad0b10e0
yann <yanndubois4@gmail.com>
2021-11-13 17:19:30 +0100

    init

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const main = async () => {
+    const orm = await core_1.MikroORM.init();
+};
+main();
+console.log('hello here');
+//# sourceMappingURL=index.js.map
